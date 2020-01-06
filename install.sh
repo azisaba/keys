c5f51690c393d2b08708e5ad534d0a251e303f93
ゆき <33691365+YukiLeafX@users.noreply.github.com>
2020-01-07 01:25:58 +0900

    エラーを無視

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 set -eux
 
-parallel -C ' ' -a mapping.txt -j 1 'adduser -D -h {2} {1}'
+parallel -C ' ' -a mapping.txt -j 1 'adduser -D -h {2} {1}' || true
 parallel -C ' ' -a mapping.txt -j 1 'printf "$(openssl rand -base64 15)\n%.0s" 1 2 | passwd {1}'
 parallel -C ' ' -a mapping.txt -j 1 'echo -n {3} | tr : "\x0" | xargs -0 -n 1 adduser {1}'
 
