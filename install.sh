87010c8d09962515357d5eff69b1903590ac9827
ゆき <33691365+YukiLeafX@users.noreply.github.com>
2020-01-21 19:56:31 +0900

    エラー無視の厳格化

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 
 set -eux
 
-parallel -C ' ' -a mapping.txt -j 1 'adduser -D -h {2} {1}' || true
+parallel -C ' ' -a mapping.txt -j 1 'adduser -D -h {2} {1} || true'
 parallel -C ' ' -a mapping.txt -j 1 'printf "$(openssl rand -base64 15)\n%.0s" 1 2 | passwd {1}'
-parallel -C ' ' -a mapping.txt -j 1 'echo -n {3} | tr : "\x0" | xargs -0 -n 1 adduser {1}' || true
+parallel -C ' ' -a mapping.txt -j 1 'echo -n {3} | tr : "\x0" | xargs -0 -n 1 adduser {1} || true'
 
 parallel -C ' ' -a mapping.txt 'mkdir -p {2}/.ssh'
 parallel -C ' ' -a mapping.txt 'chmod -c 700 {2}/.ssh'
