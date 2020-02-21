ea7162b0a0a7719cbdadeccb4a02874cfb2b50a6
ゆき <33691365+YukiLeafX@users.noreply.github.com>
2020-02-22 03:22:28 +0900

    権限修正スクリプトを作成

---
+++ perms.sh
@@ -0,0 +1,19 @@
+#!/bin/ash
+
+set -eux
+
+chmod -c 666 /opt/resourcepacks/*.zip
+setfacl -d -m u::rwX,g::rwX,o::rwX /opt/resourcepacks
+
+parallel -j 1 'addgroup -S {/} || :' ::: /opt/minecraft/*
+parallel 'find {} -type d -print0 | xargs -0 chmod -c g+s' ::: /opt/minecraft/*
+parallel 'chgrp -Rc {/} {}' ::: /opt/minecraft/*
+parallel 'chmod -c o-rwx {}' ::: /opt/minecraft/*
+parallel 'chmod -Rc g+w {}' ::: /opt/minecraft/*
+parallel 'find {} -type d -print0 | xargs -0 setfacl -d -m u::rwX,g::rwX,o::rX' ::: /opt/minecraft/*
+
+chmod -c 700 /srv/discord/logger
+chmod -c 700 /srv/discord/support
+
+chmod -c 600 /srv/secrets/wordpress.txt
+chmod -c 600 /srv/secrets/dns-cloudflare.txt
